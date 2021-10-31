bc467deb0a3e94b40f057434ed4ea2660364e666
Mercy811 <xinyiye811@gmail.com>
2021-10-31 14:06:19 -0500

    [fix] update current user list when a new user enter room

--- server.js
+++ server.js
@@ -17,26 +17,50 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
+let users = {};
+
 // 1. listen on the connection event for incoming sockets 
 //    and log it to the console
 // 2. each socket also fires a special disconnect event
 io.on('connection', (socket) => {
-  // console.log('a user connected');
+  console.log('========== connection ==========');
+  console.log(socket.id);
 
-  socket.on('join',(userInfo)=>{
-    // console.log("join callled")
-    console.log(userInfo.username);
-    console.log(userInfo.roomId);
+  socket.on('join',async (userInfo)=>{
+    console.log('========== join ==========');
+    // console.log(userInfo.username);
+    // console.log(userInfo.roomId);
+    socket.join(userInfo.roomId);
+    
+    users[socket.id] = {
+      "roomId":userInfo.roomId,
+      "username":userInfo.username
+    };
+
+    // get an array of usernames in room roomId
+    let usernamesInRoom = [];
+    await io.in(userInfo.roomId).allSockets()
+    .then(socketsInRoomSet => Array.from(socketsInRoomSet))
+    .then(socketsInRoomArray => {
+      socketsInRoomArray.forEach(element => {
+        usernamesInRoom.push(users[element].username);
+      });
+    })
+    .catch(error => console.error('Error:', error));
+
+    io.to(userInfo.roomId).emit('newUser',userInfo.username);
+    io.to(userInfo.roomId).emit('updateUserList',usernamesInRoom);
   })
 
-  // socket.on('chat message',(msg)=>{
-  //   console.log('message: '+msg);
-  //   io.emit('chat message',msg);
-  // });
+  socket.on('chat message',(msg)=>{
+    console.log('========== chat message ==========');
+    console.log(users[socket.id]);
+    io.to(users[socket.id]).emit('chat message',msg);
+  });
 
-  // socket.on('disconnect', () => {
-  //   console.log('user disconnected');
-  // });
+  socket.on('disconnect', () => {
+    console.log('========== disconnect ==========');
+  });
 });
 
 // make the http server listen on port 3000
