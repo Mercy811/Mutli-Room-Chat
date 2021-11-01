66c5aa23c2d600405fd5ba2c9f1f2746c9f5dbdc
Mercy811 <xinyiye811@gmail.com>
2021-11-01 10:56:49 -0500

    [fix] when join an existing private room, pwd required

--- server.js
+++ server.js
@@ -17,20 +17,30 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
+// users
+// socketid : 
+//      username
+//      roomId
 let users = {};
+// users
+// roomId : 
+//      owner
+//      isPrivate
+//      password
+let rooms = {};
 
-// 1. listen on the connection event for incoming sockets 
-//    and log it to the console
-// 2. each socket also fires a special disconnect event
-io.on('connection', (socket) => {
+function isRoomExist(roomId){
+  if(Object.keys(rooms).length === 0){
+    return false;
+  }else{
+    return Object.keys(rooms).includes(roomId);
+  }
+}
+
+async function userJoin(socket,userInfo){
   console.log('========== connection ==========');
-  console.log(socket.id);
-
-  socket.on('join',async (userInfo)=>{
-    console.log('========== join ==========');
-    // console.log(userInfo.username);
-    // console.log(userInfo.roomId);
-    socket.join(userInfo.roomId);
+  console.log(userInfo);
+  socket.join(userInfo.roomId);
     
     users[socket.id] = {
       "roomId":userInfo.roomId,
@@ -50,7 +60,79 @@ io.on('connection', (socket) => {
 
     io.to(userInfo.roomId).emit('newUser',userInfo.username);
     io.to(userInfo.roomId).emit('updateUserList',usernamesInRoom);
-  })
+}
+
+// 1. listen on the connection event for incoming sockets 
+//    and log it to the console
+// 2. each socket also fires a special disconnect event
+io.on('connection', async (socket) => {
+  console.log('========== connection ==========');
+  console.log(socket.id);
+
+  socket.on('createRoom',(roomInfo)=>{
+    console.log('========== createRoom ==========');
+    rooms[roomInfo.roomId] = {
+      "owner":roomInfo.username,
+      "isPrivate":roomInfo.isPrivate,
+      "password":roomInfo.password
+    }
+    console.log(rooms);
+    io.to(socket.id).emit('joinSuccess');
+    let userInfo = {username:roomInfo.username, roomId: roomInfo.roomId};
+
+    userJoin(socket, userInfo);
+  });
+
+  socket.on('join',async (userInfo)=>{
+    console.log('========== join ==========');
+    console.log(rooms);
+    if(!isRoomExist(userInfo.roomId)){
+      console.log("roomNotExist");
+      io.to(socket.id).emit('roomNotExist');
+      return;
+    }
+    
+    console.log("roomExist");
+    console.log(userInfo);
+    if(rooms[userInfo.roomId].isPrivate){
+      if(!userInfo.password){
+        console.log("passwordNeeded");
+      io.to(socket.id).emit('passwordNeeded');
+      return;
+      }else{
+        if(userInfo.password != rooms[userInfo.roomId].password){
+          console.log("wrongPassword");
+          io.to(socket.id).emit('wrongPassword');
+          return;
+        }
+      }
+    }    
+
+    io.to(socket.id).emit('joinSuccess');
+    userJoin(socket, userInfo);
+
+
+    // socket.join(userInfo.roomId);
+    
+    // users[socket.id] = {
+    //   "roomId":userInfo.roomId,
+    //   "username":userInfo.username
+    // };
+
+    // // get an array of usernames in room roomId
+    // let usernamesInRoom = [];
+    // await io.in(userInfo.roomId).allSockets()
+    // .then(socketsInRoomSet => Array.from(socketsInRoomSet))
+    // .then(socketsInRoomArray => {
+    //   socketsInRoomArray.forEach(element => {
+    //     usernamesInRoom.push(users[element].username);
+    //   });
+    // })
+    // .catch(error => console.error('Error:', error));
+
+    // io.to(userInfo.roomId).emit('newUser',userInfo.username);
+    // io.to(userInfo.roomId).emit('updateUserList',usernamesInRoom);
+  });
 
   socket.on('chat message',(msg)=>{
     io.to(users[socket.id].roomId).emit('chat message',Array(users[socket.id].username, msg));
